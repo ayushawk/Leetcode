1c781b392a10fb902f7dac2f59aeefcb37c6ebb0
ayushawk <111142859+ayushawk@users.noreply.github.com>
2024-02-02 01:34:01 +0530

    Time: 152 ms (39.22%), Space: 12.6 MB (18.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,17 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int length = nums.size();
         vector<int> arr;
-        int flag = 0;
+
         for(int i=0; i<length-1; i++){
             for(int j=i+1; j<length; j++){
                 if((nums[i] + nums[j]) == target){
-                    flag = 1; 
                     arr.push_back(i);
                     arr.push_back(j);
+                    break;
                 }
-            }
-            if(flag == 1){
-                break;
             }
         }
         return arr;
